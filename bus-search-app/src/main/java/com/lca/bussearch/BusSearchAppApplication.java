92cdd48dd2fc9a75ee97d6849b55bed0723c030a
Sandeep Patange <sandeep.upatange@gmail.com>
2025-03-23 15:16:15 +0530

    created a multi module project  with bus-search-app, real-estate-app, advertise-app

--- bus-search-app/src/main/java/com/lca/bussearch/BusSearchAppApplication.java
+++ bus-search-app/src/main/java/com/lca/bussearch/BusSearchAppApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusTimingAppApplication {
+public class BusSearchAppApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BusTimingAppApplication.class, args);
+        SpringApplication.run(BusSearchAppApplication.class, args);
     }
 }
